844d2e2aaf0453beb986bc951d8af7d6d74b432e
interysy <2554268w@student.gla.ac.uk>
2024-10-28 10:45:25 +0000

    ci(logger): adding enrichers
    adding enrichers to display:
    * machine name
    * ip
    * application name
    
    along with other information from serilog.
    This is all displayed in all logs, which are shown in the console and the logs directory.

---
+++ LibraryManagementSystemV2/Configuration/LoggerSetup.cs
@@ -0,0 +1,41 @@
+﻿using Serilog;
+using Microsoft.Extensions.Configuration;
+using ILogger = Serilog.ILogger;
+
+namespace LibraryManagementSystemV2.Configuration
+{
+    public static class LoggerSetup
+    {
+        const string LogFilePrefix = "logs/log_run_";
+        const string LogFileSuffix = ".txt";
+        const string OutputTemplate = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj} (App: {ApplicationName}, Env: {Environment}, Machine : {MachineName}, IP : {ClientIp}){NewLine}{Exception}";
+
+
+        public static ILogger SetUpLogger(IConfiguration configuration)
+        {
+            System.IO.Directory.CreateDirectory("logs");
+
+            string runName = LogFilePrefix + currentDateAsName() + LogFileSuffix;
+
+            string? environment = configuration["SerilogEnrichers:Environment"];
+            string? appName = configuration["SerilogEnrichers:ApplicationName"];
+
+            Log.Logger = new LoggerConfiguration()
+                        .MinimumLevel.Debug()
+                        .Enrich.WithProperty("Environment", environment ?? "Unknown")
+                        .Enrich.WithProperty("ApplicationName", appName ?? "Unknown")
+                        .Enrich.WithMachineName()
+                        .Enrich.WithClientIp()
+                        .WriteTo.Console(outputTemplate: OutputTemplate)
+                        .WriteTo.File(runName, outputTemplate: OutputTemplate)
+                        .CreateLogger();
+
+            return Log.Logger;
+        }
+
+        public static string currentDateAsName()
+        {
+            return DateTime.UtcNow.ToString("yyyy-MM-dd-HH-mm-ss");
+        }
+    }
+}
--- LibraryManagementSystemV2/Program.cs
+++ LibraryManagementSystemV2/Program.cs
@@ -6,26 +6,28 @@ using LibraryManagementSystemV2.Services;
 using LibraryManagementSystemV2.Mappings;
 using LibraryManagementSystemV2.Repositories;
 using Serilog;
+using System;
+using LibraryManagementSystemV2.Configuration;
+using System.Configuration;
+using Microsoft.Extensions.Hosting;
+using LibraryManagementSystemV2.Services.GenericServiceMappings;
 
 
 var builder = WebApplication.CreateBuilder(args);
 var databaseName = "LibraryManagmentSystem";
-// Add services to the container.  
+ 
 
 builder.Services.AddAutoMapper(typeof(MappingProfile));
 
-//// Generic Repository
+
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddScoped(typeof(IUnitOfWork), typeof(UnitOfWork));
 
-//// Generic Services
+
 builder.Services.AddScoped(typeof(IReadService<,>), typeof(ReadService<,>));
 builder.Services.AddScoped(typeof(IGenericService<,,,>), typeof(GenericService<,,,>));
 
 
-//////////////////////////////////// Services ////////////////////////////////////
-
-// Asset Mappings
 builder.Services.AddScoped(typeof(ITestBookMapping), typeof(TestBookService));
 
 builder.Services.AddScoped<IBookAuthorService, BookAuthorsService>(); 
@@ -34,33 +36,23 @@ builder.Services.AddScoped<IRenterService, RenterService>();
 builder.Services.AddScoped<IRentalService, RentalService>();
 builder.Services.AddScoped<IAuthorService, AuthorService>();
 
+builder.Services.AddHttpContextAccessor();
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<LibraryManagementContext>();
 builder.Services.AddDbContext<SQLiteContext>();
-//builder.Services.AddDbContext<BookContext>();
-//builder.Services.AddDbContext<RenterContext>();
-//builder.Services.AddDbContext<LibraryContext>(opt =>
-//    opt.UseInMemoryDatabase(databaseName));
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle 
-//builder.Services.AddDbContext<BookContext>(opt =>
-//    opt.UseInMemoryDatabase(databaseName));
-//builder.Services.AddDbContext<EntityContext>(opt =>
-//    opt.UseInMemoryDatabase(databaseName));
-//builder.Services.AddDbContext<AuthorContext>(opt =>
-//    opt.UseInMemoryDatabase(databaseName));
-//builder.Services.AddDbContext<RenterContext>(opt =>
-//    opt.UseInMemoryDatabase(databaseName));
-//builder.Services.AddDbContext<RentalContext>(opt =>
-//    opt.UseInMemoryDatabase(databaseName));
-//builder.Services.AddDbContext<AuthorBookContext>(opt =>
-//    opt.UseInMemoryDatabase(databaseName));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+var configuration = builder.Configuration;
+
+
+var logger = LoggerSetup.SetUpLogger(configuration);
+builder.Host.UseSerilog(logger);
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -74,15 +66,11 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-Log.Logger = new LoggerConfiguration()
-    .MinimumLevel.Debug()
-    .WriteTo.Console()
-    .CreateLogger();
-
 try
 {
     Log.Information("Starting application ..."); 
     await app.RunAsync();
+    Log.Information("App started ...");
 }
 catch (Exception exception)
 {
